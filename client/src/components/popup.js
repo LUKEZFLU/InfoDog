aeb6a23f7cf2a438aebce523e0e49c36531a643c
ZefanLu <71050639+LUKEZFLU@users.noreply.github.com>
2024-04-04 20:48:11 -0700

    add pop up components

---
+++ client/src/components/popup.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './popup.css'
+
+function Popup(props) {
+    return (props.trigger) ? ( // show the following if the trigger is true
+        <div className="popup">
+            <div className='popup-inner'>
+                <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
+                {props.children}
+            </div>
+        </div>
+    ) : "";
+}
+
+export default Popup
+
+
+
+// Below is a example of using this component
+// import Popup from './components/Popup';    // import this component
+// import {useState} from 'react';    // must import use state
+
+
+// function App() {
+//   const [buttonPopup, setButtonPopup] = useState(false);   //setting
+//   return (
+//     <div className="App">
+//       <main>
+//         <h1>React App!</h1>
+//         <br></br>
+//         <button onClick={() => setButtonPopup(true)}>Open Popup</button>   // add setButtonPopup(true) to give a chance to open
+
+//       </main>
+
+//       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>     // implement this popup
+//         <h3>Well done!</h3> // Title
+//         <p>Stay Patient for people to send your contact requests. </p> // Message
+//       </Popup>
+
+//     </div>
+//   );
+// }
+
+// export default App;
